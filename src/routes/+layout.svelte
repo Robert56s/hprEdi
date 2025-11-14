00ee19ceab4e986270607c55548741ba67d04706
Robert56s <78372422+Robert56s@users.noreply.github.com>
2025-11-13 21:28:40 -0500

    second

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,35 @@
 <script>
 	import favicon from '$lib/assets/favicon.svg';
+  import { nonpassive } from 'svelte/legacy';
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	<style>
+		@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap');
+		body {
+			margin: 0;
+			font-family: "Roboto", sans-serif;
+			font-optical-sizing: auto;
+			font-weight: <weight>;
+			font-style: normal;
+			font-variation-settings:
+			"wdth" 100;
+		}
+	</style>
 </svelte:head>
 
-{@render children()}
+<div class="app">
+	{@render children()}
+</div>
+
+<style>
+	.app {
+		height: 100vh;
+		width: 100vw;
+		display: flex;
+		flex-direction: column;
+	}
+</style>
